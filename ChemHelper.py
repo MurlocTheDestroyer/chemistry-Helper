3d5183f7be14b1403bbbbe14671262f788c5f5dc
MurlocTheDestroyer <DHuddleston01@indianatech.net>
2026-04-21 19:34:05 -0400

    Hope this works

---
+++ ChemHelper.py
@@ -0,0 +1,86 @@
+import time
+import PeriodicTable as PT
+class main ():
+    def __init__(self) -> None:
+        pass
+
+    def chemical_Lookup():
+        chemical=input("What chemical would you like?:\t").capitalize()
+        if chemical in PT.Chemicals:
+            fieldData=input(f"What data do you want from the chemical [{chemical}]?\n 1.Symbol\n 2.Atomic Mass\n 3.Atomic Number\n Enter a number or hit the ENTER key for all fields of data:\t")
+            for i in PT.Chemicals:
+                match fieldData:
+                    case "1" if chemical == i:
+                        return (print(f'\nThe Symbol of the chemical {i} is {PT.Chemicals[i]["Symbol"]}\n'))
+                    case "2" if chemical == i:
+                        return (print(f'\nThe Atomic Mass of the chemical {i} is {PT.Chemicals[i]["AtomicMass"]}\n'))
+                    case "3" if chemical == i:
+                        return (print(f'\nThe Atomic Number of the chemical {i} is {PT.Chemicals[i]["AtomicNumber"]}\n'))
+                    case _ if chemical == i:
+                        return (print(f'#{PT.Chemicals[i]["AtomicNumber"]} | {i}\n\tSymbol | {PT.Chemicals[i]["Symbol"]}\n\tAtomic Mass | {PT.Chemicals[i]["AtomicMass"]}\n\tAtomic Number | {PT.Chemicals[i]["AtomicNumber"]}\n'))
+                #used for testing purposes dont include in stuff
+                #print("Incorrect Chemical match")
+        else:
+            return (print(f"Yikers. The chemical with the name {chemical} isnt in the file currently."))
+
+    def launch_info():
+        print("List of Chemicals and current info")
+        print("___________________________________________________")
+        for i in PT.Chemicals:
+            print(f' #{PT.Chemicals[i]["AtomicNumber"]} | {i}\n\tSymbol | {PT.Chemicals[i]["Symbol"]}\n\tAtomic Mass | {PT.Chemicals[i]["AtomicMass"]}\n\tAtomic Number | {PT.Chemicals[i]["AtomicNumber"]}\n')
+            time.sleep(0.1)
+
+    def chemical_Add():
+        # desired outcome if elements are put in {combined symbols} and {combined mass} EX: hydrogen hydrogen == H2, 2.016g
+        SymbolAdd={}
+        combinedmass=0
+        i=1
+        while True:
+            chemical=input(f"Please input name of chemical to add\nChemical #{i}:")
+            chemical=chemical.capitalize()
+            if chemical == "Quit" or chemical == "Stop":
+                break
+            else:
+                if chemical in PT.Chemicals:
+                    numberUsed=int(input(f"Please input how many atoms of {chemical} are in the solution: "))
+                    for j in PT.Chemicals:
+                        if j==chemical:
+                            if PT.Chemicals[j]["Symbol"] in SymbolAdd:
+                                SymbolAdd.update({(PT.Chemicals[j]["Symbol"]):(SymbolAdd.get[PT.Chemicals[j]["Symbol"]] + numberUsed)})
+                            else:
+                                SymbolAdd.update({PT.Chemicals[j]["Symbol"]:numberUsed})
+
+                            combinedmass+=((PT.Chemicals[j]["AtomicMass"])*numberUsed)
+                            
+                            if numberUsed == 1:
+                                print(f'{numberUsed} atom of {j} added to solution')
+                            else:
+                                print(f'{numberUsed} atoms of {j} added to solution')
+                            
+                    i+=1
+                else:
+                    print(f"Yikers. The chemical with the name {chemical} isnt in the file currently. Please try again.")
+        return (print(f"This is the dictionary :{SymbolAdd}"), print(f"This is the combined mass for dictionary :{combinedmass}g"))
+    
+    def bootup():
+        choice=input("What Command?\t Current commands (launch_info),(chemical_Lookup),(chemical_Add)\n")
+        match choice.lower():
+            case "launch_info":
+                print(f"Running Command: {choice}")
+                main.launch_info()
+                print(f"Command ({choice}) has completed")
+            case "chemical_lookup":
+                print(f"Running Command: {choice}")
+                main.chemical_Lookup()
+                print(f"Command ({choice}) has completed")
+            case "chemical_add":
+                print(f"Running Command: {choice}")
+                main.chemical_Add()
+                print(f"Command ({choice}) has completed")
+            case "dismiss" | "quit":
+                return (print("As you wish. Good Bye!"))
+            case _:
+                print(f" ({choice}) COMMAND NOT FOUND. Please try again")
+                main.bootup()
+
+main.chemical_Add()
---
+++ PeriodicTable.py
@@ -0,0 +1,713 @@
+Hydrogen = {
+    "Symbol":"H",
+    "AtomicMass":1.008,
+    "AtomicNumber":1
+}
+
+Helium = {
+    "Symbol":"He",
+    "AtomicMass":4.0026,
+    "AtomicNumber":2
+}
+Lithium = {
+    "Symbol":"Li",
+    "AtomicMass":6.94,
+    "AtomicNumber":3
+}
+Beryllium = {
+    "Symbol":"Be",
+    "AtomicMass":9.0122,
+    "AtomicNumber":4
+}
+Boron = {
+    "Symbol":"B",
+    "AtomicMass":10.81,
+    "AtomicNumber":5
+}
+Carbon = {
+    "Symbol":"C",
+    "AtomicMass":12.011,
+    "AtomicNumber":6
+}
+Nitrogen = {
+    "Symbol":"N",
+    "AtomicMass":14.007,
+    "AtomicNumber":7
+}
+Oxygen = {
+    "Symbol":"O",
+    "AtomicMass":15.999,
+    "AtomicNumber":8
+}
+Fluorine = {
+    "Symbol":"F",
+    "AtomicMass":18.998,
+    "AtomicNumber":9
+}
+Neon = {
+    "Symbol":"Ne",
+    "AtomicMass":20.180,
+    "AtomicNumber":10
+}
+Sodium = {
+    "Symbol":"Na",
+    "AtomicMass":22.990,
+    "AtomicNumber":11
+}
+Magnesium = {
+    "Symbol":"Mg",
+    "AtomicMass":24.305,
+    "AtomicNumber":12
+}
+Aluminium = {
+    "Symbol":"Al",
+    "AtomicMass":26.982,
+    "AtomicNumber":13
+}
+Silicon = {
+    "Symbol":"Si",
+    "AtomicMass":28.085,
+    "AtomicNumber":14
+}
+Phosphorus = {
+    "Symbol":"P",
+    "AtomicMass":30.974,
+    "AtomicNumber":15
+}
+Sulfur = {
+    "Symbol":"S",
+    "AtomicMass":32.06,
+    "AtomicNumber":16
+}
+Chlorine = {
+    "Symbol":"Cl",
+    "AtomicMass":35.45,
+    "AtomicNumber":17
+}
+Argon = {
+    "Symbol":"Ar",
+    "AtomicMass":39.948,
+    "AtomicNumber":18
+}
+Potassium = {
+    "Symbol":"K",
+    "AtomicMass":39.098,
+    "AtomicNumber":19
+}
+Calcium = {
+    "Symbol":"Ca",
+    "AtomicMass":40.078,
+    "AtomicNumber":20
+}
+Scandium = {
+    "Symbol":"Sc",
+    "AtomicMass":44.956,
+    "AtomicNumber":21
+}
+Titanium = {
+    "Symbol":"Ti",
+    "AtomicMass":47.867,
+    "AtomicNumber":22
+}
+Vanadium = {
+    "Symbol":"V",
+    "AtomicMass":50.942,
+    "AtomicNumber":23
+}
+Chromium = {
+    "Symbol":"Cr",
+    "AtomicMass":51.996,
+    "AtomicNumber":24
+}
+Manganese = {
+    "Symbol":"Mn",
+    "AtomicMass":54.938,
+    "AtomicNumber":25
+}
+Iron = {
+    "Symbol":"Fe",
+    "AtomicMass":55.845,
+    "AtomicNumber":26
+}
+Cobalt = {
+    "Symbol":"Co",
+    "AtomicMass":58.933,
+    "AtomicNumber":27
+}
+Nickel = {
+    "Symbol":"Ni",
+    "AtomicMass":58.693,
+    "AtomicNumber":28
+}
+Copper = {
+    "Symbol":"Cu",
+    "AtomicMass":63.546,
+    "AtomicNumber":29
+}
+Zinc = {
+    "Symbol":"Zn",
+    "AtomicMass":65.38,
+    "AtomicNumber":30
+}
+Gallium = {
+    "Symbol":"Ga",
+    "AtomicMass":69.723,
+    "AtomicNumber":31
+}
+Germanium = {
+    "Symbol":"Ge",
+    "AtomicMass":72.63,
+    "AtomicNumber":32
+}
+Arsenic = {
+    "Symbol":"As",
+    "AtomicMass":74.92,
+    "AtomicNumber":33
+}
+Selenium = {
+    "Symbol":"Se",
+    "AtomicMass":78.96,
+    "AtomicNumber":34
+}
+Bromine = {
+    "Symbol":"Br",
+    "AtomicMass":79.90,
+    "AtomicNumber":35
+}
+Krypton = {
+    "Symbol":"Kr",
+    "AtomicMass":83.80,
+    "AtomicNumber":36
+}
+Rubidium = {
+    "Symbol":"Rb",
+    "AtomicMass":85.47,
+    "AtomicNumber":37
+}
+Strontium = {
+    "Symbol":"Sr",
+    "AtomicMass":87.62,
+    "AtomicNumber":38
+}
+Yttrium = {
+    "Symbol":"Y",
+    "AtomicMass":88.91,
+    "AtomicNumber":39
+}
+Zirconium = {
+    "Symbol":"Zr",
+    "AtomicMass":91.22,
+    "AtomicNumber":40
+}
+Niobium = {
+    "Symbol":"Nb",
+    "AtomicMass":92.91,
+    "AtomicNumber":41
+}
+Molybdenum = {
+    "Symbol":"Mo",
+    "AtomicMass":95.94,
+    "AtomicNumber":42
+}
+Technetium = {
+    "Symbol":"Tc",
+    "AtomicMass":98.00,
+    "AtomicNumber":43
+}
+Ruthenium = {
+    "Symbol":"Ru",
+    "AtomicMass":101.10,
+    "AtomicNumber":44
+}
+Rhodium = {
+    "Symbol":"Rh",
+    "AtomicMass":102.90,
+    "AtomicNumber":45
+}
+Palladium = {
+    "Symbol":"Pd",
+    "AtomicMass":106.40,
+    "AtomicNumber":46
+}
+Silver = {
+    "Symbol":"Ag",
+    "AtomicMass":107.90,
+    "AtomicNumber":47
+}
+Cadmium = {
+    "Symbol":"Cd",
+    "AtomicMass":112.40,
+    "AtomicNumber":48
+}
+Indium = {
+    "Symbol":"In",
+    "AtomicMass":114.80,
+    "AtomicNumber":49
+}
+Tin = {
+    "Symbol":"Sn",
+    "AtomicMass":118.70,
+    "AtomicNumber":50
+}
+Antimony = {
+    "Symbol":"Sb",
+    "AtomicMass":121.8,
+    "AtomicNumber":51
+}
+Tellurium = {
+    "Symbol":"Te",
+    "AtomicMass":127.6,
+    "AtomicNumber":52
+}
+Iodine = {
+    "Symbol":"I",
+    "AtomicMass":126.9,
+    "AtomicNumber":53
+}
+Xenon = {
+    "Symbol":"Xe",
+    "AtomicMass":131.3,
+    "AtomicNumber":54
+}
+Cesium = {
+    "Symbol":"Cs",
+    "AtomicMass":132.9,
+    "AtomicNumber":55
+}
+Barium = {
+    "Symbol":"Ba",
+    "AtomicMass":137.3,
+    "AtomicNumber":56
+}
+Lanthanum = {
+    "Symbol":"La",
+    "AtomicMass":138.9,
+    "AtomicNumber":57
+}
+Cerium = {
+    "Symbol":"Ce",
+    "AtomicMass":140.1,
+    "AtomicNumber":58
+}
+Praseodymium = {
+    "Symbol":"Pr",
+    "AtomicMass":140.9,
+    "AtomicNumber":59
+}
+Neodymium = {
+    "Symbol":"Nd",
+    "AtomicMass":144.2,
+    "AtomicNumber":60
+}
+Promethium = {
+    "Symbol":"Pm",
+    "AtomicMass":145,
+    "AtomicNumber":61
+}
+Samarium = {
+    "Symbol":"Sm",
+    "AtomicMass":150.4,
+    "AtomicNumber":62
+}
+Europium = {
+    "Symbol":"Eu",
+    "AtomicMass":152.0,
+    "AtomicNumber":63
+}
+Gadolinium = {
+    "Symbol":"Gd",
+    "AtomicMass":157.3,
+    "AtomicNumber":64
+}
+Terbium = {
+    "Symbol":"Tb",
+    "AtomicMass":158.9,
+    "AtomicNumber":65
+}
+Dysprosium = {
+    "Symbol":"Dy",
+    "AtomicMass":162.5,
+    "AtomicNumber":66
+}
+Holmium = {
+    "Symbol":"Ho",
+    "AtomicMass":164.9,
+    "AtomicNumber":67
+}
+Erbium = {
+    "Symbol":"Er",
+    "AtomicMass":167.3,
+    "AtomicNumber":68
+}
+Thulium = {
+    "Symbol":"Tm",
+    "AtomicMass":168.90,
+    "AtomicNumber":69
+}
+Ytterbium = {
+    "Symbol":"Yb",
+    "AtomicMass":173.0,
+    "AtomicNumber":70
+}
+Lutetium = {
+    "Symbol":"Lu",
+    "AtomicMass":175.0,
+    "AtomicNumber":71
+}
+Hafnium = {
+    "Symbol":"Hf",
+    "AtomicMass":178.5,
+    "AtomicNumber":72
+}
+Tantalum = {
+    "Symbol":"Ta",
+    "AtomicMass":180.9,
+    "AtomicNumber":73
+}
+Tungsten = {
+    "Symbol":"W",
+    "AtomicMass":183.8,
+    "AtomicNumber":74
+}
+Rhenium = {
+    "Symbol":"Re",
+    "AtomicMass":186.2,
+    "AtomicNumber":75
+}
+Osmium = {
+    "Symbol":"Os",
+    "AtomicMass":190.2,
+    "AtomicNumber":76
+}
+Iridium = {
+    "Symbol":"Ir",
+    "AtomicMass":192.2,
+    "AtomicNumber":77
+}
+Platinum = {
+    "Symbol":"Pt",
+    "AtomicMass":195.1,
+    "AtomicNumber":78
+}
+Gold = {
+    "Symbol":"Au",
+    "AtomicMass":197.0,
+    "AtomicNumber":79
+}
+Mercury = {
+    "Symbol":"Hg",
+    "AtomicMass":200.6,
+    "AtomicNumber":80
+}
+Thallium = {
+    "Symbol":"Tl",
+    "AtomicMass":204.4,
+    "AtomicNumber":81
+}
+Lead = {
+    "Symbol":"Pb",
+    "AtomicMass":207.2,
+    "AtomicNumber":82
+}
+Bismuth = {
+    "Symbol":"Bi",
+    "AtomicMass":209.0,
+    "AtomicNumber":83
+}
+Polonium = {
+    "Symbol":"Po",
+    "AtomicMass":209,
+    "AtomicNumber":84
+}
+Astatine = {
+    "Symbol":"At",
+    "AtomicMass":210,
+    "AtomicNumber":85
+}
+Radon = {
+    "Symbol":"Rn",
+    "AtomicMass":222,
+    "AtomicNumber":86
+}
+Francium = {
+    "Symbol":"Fr",
+    "AtomicMass":223,
+    "AtomicNumber":87
+}
+Radium = {
+    "Symbol":"Ra",
+    "AtomicMass":226,
+    "AtomicNumber":88
+}
+Actinium = {
+    "Symbol":"Ac",
+    "AtomicMass":227,
+    "AtomicNumber":89
+}
+Thorium = {
+    "Symbol":"Th",
+    "AtomicMass":232.0,
+    "AtomicNumber":90
+}
+Protactinium = {
+    "Symbol":"Pa",
+    "AtomicMass":231.0,
+    "AtomicNumber":91
+}
+Uranium = {
+    "Symbol":"U",
+    "AtomicMass":238.0,
+    "AtomicNumber":92
+}
+Neptunium = {
+    "Symbol":"Np",
+    "AtomicMass":237,
+    "AtomicNumber":93
+}
+Plutonium = {
+    "Symbol":"Pu",
+    "AtomicMass":244,
+    "AtomicNumber":94
+}
+Americium = {
+    "Symbol":"Am",
+    "AtomicMass":243,
+    "AtomicNumber":95
+}
+Curium = {
+    "Symbol":"Cm",
+    "AtomicMass":247,
+    "AtomicNumber":96
+}
+Berkelium = {
+    "Symbol":"Bk",
+    "AtomicMass":247,
+    "AtomicNumber":97
+}
+Californium = {
+    "Symbol":"Cf",
+    "AtomicMass":251,
+    "AtomicNumber":98
+}
+Einsteinium = {
+    "Symbol":"Es",
+    "AtomicMass":252,
+    "AtomicNumber":99
+}
+Fermium = {
+    "Symbol":"Fm",
+    "AtomicMass":257,
+    "AtomicNumber":100
+}
+Mendelevium = {
+    "Symbol":"Md",
+    "AtomicMass":258,
+    "AtomicNumber":101
+}
+Nobelium = {
+    "Symbol":"No",
+    "AtomicMass":259,
+    "AtomicNumber":102
+}
+Lawrencium = {
+    "Symbol":"Lr",
+    "AtomicMass":262,
+    "AtomicNumber":103
+}
+Rutherfordium = {
+    "Symbol":"Rf",
+    "AtomicMass":261,
+    "AtomicNumber":104
+}
+Dubnium = {
+    "Symbol":"Db",
+    "AtomicMass":262,
+    "AtomicNumber":105
+}
+Seaborgium = {
+    "Symbol":"Sg",
+    "AtomicMass":266,
+    "AtomicNumber":106
+}
+Bohrium = {
+    "Symbol":"Bh",
+    "AtomicMass":264,
+    "AtomicNumber":107
+}
+Hassium = {
+    "Symbol":"Hs",
+    "AtomicMass":277,
+    "AtomicNumber":108
+}
+Meitnerium = {
+    "Symbol":"Mt",
+    "AtomicMass":268,
+    "AtomicNumber":109
+}
+Darmstadtium = {
+    "Symbol":"Ds",
+    "AtomicMass":271,
+    "AtomicNumber":110
+}
+Roentgenium = {
+    "Symbol":"Rg",
+    "AtomicMass":272,
+    "AtomicNumber":111
+}
+Copernicium = {
+    "Symbol":"Cn",
+    "AtomicMass":285,
+    "AtomicNumber":112
+}
+Nihonium = {
+    "Symbol":"Nh",
+    "AtomicMass":286,
+    "AtomicNumber":113
+}
+Flerovium = {
+    "Symbol":"Fl",
+    "AtomicMass":289,
+    "AtomicNumber":114
+}
+Moscovium = {
+    "Symbol":"Mc",
+    "AtomicMass":290,
+    "AtomicNumber":115
+}
+Livermorium = {
+    "Symbol":"Lv",
+    "AtomicMass":293,
+    "AtomicNumber":116
+}
+Tennessine = {
+    "Symbol":"Ts",
+    "AtomicMass":294,
+    "AtomicNumber":117
+}
+Oganesson = {
+    "Symbol":"Og",
+    "AtomicMass":294,
+    "AtomicNumber":118
+}
+
+Chemicals={
+    "Hydrogen": Hydrogen,
+    "Helium": Helium,
+    "Lithium": Lithium,
+    "Beryllium": Beryllium,
+    "Boron": Boron,
+    "Carbon": Carbon,
+    "Nitrogen": Nitrogen,
+    "Oxygen": Oxygen,
+    "Fluorine": Fluorine,
+    "Neon": Neon,
+    "Sodium": Sodium,
+    "Magnesium": Magnesium,
+    "Aluminium": Aluminium,
+    "Silicon": Silicon,
+    "Phosphorus": Phosphorus,
+    "Sulfur": Sulfur,
+    "Chlorine": Chlorine,
+    "Argon": Argon,
+    "Potassium": Potassium,
+    "Calcium": Calcium,
+    "Scandium": Scandium,
+    "Titanium": Titanium,
+    "Vanadium": Vanadium,
+    "Chromium": Chromium,
+    "Manganese": Manganese,
+    "Iron": Iron,
+    "Cobalt": Cobalt,
+    "Nickel": Nickel,
+    "Copper": Copper,
+    "Zinc": Zinc,
+    "Gallium": Gallium,
+    "Germanium": Germanium,
+    "Arsenic": Arsenic,
+    "Selenium": Selenium,
+    "Bromine": Bromine,
+    "Krypton": Krypton,
+    "Rubidium": Rubidium,
+    "Strontium": Strontium,
+    "Yttrium": Yttrium,
+    "Zirconium": Zirconium,
+    "Niobium": Niobium,
+    "Molybdenum": Molybdenum,
+    "Technetium": Technetium,
+    "Ruthenium": Ruthenium,
+    "Rhodium": Rhodium,
+    "Palladium": Palladium,
+    "Silver": Silver,
+    "Cadmium": Cadmium,
+    "Indium": Indium,
+    "Tin": Tin,
+    "Antimony": Antimony,
+    "Tellurium": Tellurium,
+    "Iodine": Iodine,
+    "Xenon": Xenon,
+    "Cesium": Cesium,
+    "Barium": Barium,
+    "Lanthanum": Lanthanum,
+    "Cerium": Cerium,
+    "Praseodymium": Praseodymium,
+    "Neodymium": Neodymium,
+    "Promethium": Promethium,
+    "Samarium": Samarium,
+    "Europium": Europium,
+    "Gadolinium": Gadolinium,
+    "Terbium": Terbium,
+    "Dysprosium": Dysprosium,
+    "Holmium": Holmium,
+    "Erbium": Erbium,
+    "Thulium": Thulium,
+    "Ytterbium": Ytterbium,
+    "Lutetium": Lutetium,
+    "Hafnium": Hafnium,
+    "Tantalum": Tantalum,
+    "Tungsten": Tungsten,
+    "Rhenium": Rhenium,
+    "Osmium": Osmium,
+    "Iridium": Iridium,
+    "Platinum": Platinum,
+    "Gold": Gold,
+    "Mercury": Mercury,
+    "Thallium": Thallium,
+    "Lead": Lead,
+    "Bismuth": Bismuth,
+    "Polonium": Polonium,
+    "Astatine": Astatine,
+    "Radon": Radon,
+    "Francium": Francium,
+    "Radium": Radium,
+    "Actinium": Actinium,
+    "Thorium": Thorium,
+    "Protactinium": Protactinium,
+    "Uranium": Uranium,
+    "Neptunium": Neptunium,
+    "Plutonium": Plutonium,
+    "Americium": Americium,
+    "Curium": Curium,
+    "Berkelium": Berkelium,
+    "Californium": Californium,
+    "Einsteinium": Einsteinium,
+    "Fermium": Fermium,
+    "Mendelevium": Mendelevium,
+    "Nobelium": Nobelium,
+    "Lawrencium": Lawrencium,
+    "Rutherfordium": Rutherfordium,
+    "Dubnium": Dubnium,
+    "Seaborgium": Seaborgium,
+    "Bohrium": Bohrium,
+    "Hassium": Hassium,
+    "Meitnerium": Meitnerium,
+    "Darmstadtium": Darmstadtium,
+    "Roentgenium": Roentgenium,
+    "Copernicium": Copernicium,
+    "Nihonium": Nihonium,
+    "Flerovium": Flerovium,
+    "Moscovium": Moscovium,
+    "Livermorium": Livermorium,
+    "Tennessine": Tennessine,
+    "Oganesson": Oganesson
+    
+}
